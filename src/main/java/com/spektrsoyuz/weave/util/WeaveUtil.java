0858bee4e382403431efd5f1ef958aceed7bf8e2
SpektrSoyuz <christies1109@gmail.com>
2025-01-23 12:05:11 -0500

    Add PAPI tag resolver for minimessage

--- src/main/java/com/spektrsoyuz/weave/util/WeaveUtil.java
+++ src/main/java/com/spektrsoyuz/weave/util/WeaveUtil.java
@@ -5,6 +5,13 @@
  */
 package com.spektrsoyuz.weave.util;
 
+import me.clip.placeholderapi.PlaceholderAPI;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.tag.Tag;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
 public class WeaveUtil {
     /* Permission Nodes */
     public static final String PERMISSION_COMMAND_NICKNAME = "cc.command.nickname";
@@ -17,4 +24,15 @@ public class WeaveUtil {
     public static final String MESSAGE_COMMAND_NICKNAME_VIEW = "<gold>Your current nickname is:</gold> <nickname><gold>.\nSet your nickname with /nickname <nickname>.</gold>";
     public static final String MESSAGE_COMMAND_NICKNAME_RESET = "<gold>Nickname has been reset.</gold>";
     public static final String MESSAGE_COMMAND_NICKNAME_SET = "<gold>Nickname has been set to</gold><nickname><gold>.</gold>";
+
+    /* Config Paths */
+
+    /* MiniMessage Resolvers */
+    public static @NotNull TagResolver papiTag(final @NotNull Player player) {
+        return TagResolver.resolver("papi", (argumentQueue, context) -> {
+            String placeholder = argumentQueue.popOr("papi tag requires an argument").value();
+            String parsed = PlaceholderAPI.setPlaceholders(player, '%' + placeholder + '%');
+            return Tag.selfClosingInserting(Component.text(parsed));
+        });
+    }
 }
